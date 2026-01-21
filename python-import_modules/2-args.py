b3258bd03300b168deede38f211656abcbc9e680
“Shan” <liangyongshan33@gmail.com>
2026-01-21 22:53:44 +0000

    remove new lines and fix range

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,12 +3,12 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) == 1:
-        print("0 arguments.\n")
+        print("0 arguments.")
     elif len(argv) == 2:
-        print("1 argument:\n")
+        print("1 argument:")
         print("1: {}".format(argv[1]))
     elif len(argv) > 2:
-        print("{} arguments:\n".format(len(argv) - 1))
-        for i in range(len(argv)):
-            print("{}: {}\n".format(i + 1, argv[i + 1]))
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(len(argv) - 1):
+            print("{}: {}".format(i + 1, argv[i + 1]))
             i += 1
