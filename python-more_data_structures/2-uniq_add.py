488efc286076c46f9fbba4389d0ed13a94fd0605
“Shan” <liangyongshan33@gmail.com>
2026-01-29 22:11:43 +0000

    add 2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    total = 0
+    for i in range(len(my_list)):
+        if my_list[i] not in new_list:
+            new_list.append(my_list[i])
+            total += my_list[i]
+    return total
