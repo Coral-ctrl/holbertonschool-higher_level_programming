e3530efa4d6010f826f85244b2d96148ff9fc390
“Shan” <liangyongshan33@gmail.com>
2026-02-09 05:07:48 +0000

    restructure

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,8 +11,7 @@ def inherits_from(obj, a_class):
     object is an instance of a class that inherited from, the specified class.
     """
 
-    if isinstance(obj, a_class):
-        if type(obj) is not a_class:
+    if isinstance(obj, a_class) and type(obj) is not a_class:
             return True
     else:
         return False
