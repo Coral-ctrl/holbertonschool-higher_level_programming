a4a0102f290efca70c6b7819e10b2535b795561a
“Shan” <liangyongshan33@gmail.com>
2026-02-12 04:56:15 +0000

    add blank line

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -39,6 +39,7 @@ class Circle(Shape):
         """Calculate the perimeter of the circle."""
         return 2 * math.pi * self.radius
 
+
 class Rectangle(Shape):
     """Concrete implementation of a Rectangle shape."""
 
