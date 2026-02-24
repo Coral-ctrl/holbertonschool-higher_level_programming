161cd76b90d007eff0d0fe488b459cec5aede2aa
“Shan” <liangyongshan33@gmail.com>
2026-02-24 22:43:32 +0000

    Develop a Simple API using Python with Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Module that develop Simple API with Flask"""
+
+
+from flask import Flask
+from flask import jsonify
+
+# Create an instance of class Flask
+# argument: name of application's module or package
+# so Flask knows where to look for resources like templates
+app = Flask(__name__)
+
+# route decorator to tell Flask what URL should trigger function
+@app.route("/")
+def home():
+    """function that returns message to display in browser"""
+    return "<p>Welcome to the Flask API!</p>"
+
+@app.route("/data")
+def data():
+    return jsonify(list(users.key()))
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<username>")
+def get_user(username):
+    user = users.get(username)
+    if user is None:
+        return jsonify({"error": "User not found"}), 404
+    return jsonify(user)
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({"error": "Invalid JSON"}), 400
+    username = data.get("username")
+    if not username:
+        return jsonify({"error": "Username is requires"}), 400
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 409
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
+
+if __name__ == "__main__":
+    app.run()
