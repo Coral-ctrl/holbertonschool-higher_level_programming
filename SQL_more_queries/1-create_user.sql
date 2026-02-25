fd438cdcc1aef0ddf07ca174e2d2d7329ced1af3
“Shan” <liangyongshan33@gmail.com>
2026-02-25 05:03:01 +0000

    Root user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create user with password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on the server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
