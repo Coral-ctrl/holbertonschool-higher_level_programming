512d0692e847db7b5c4146f62373f1434193de98
“Shan” <liangyongshan33@gmail.com>
2026-02-25 03:46:35 +0000

    fix

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Print description of table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE table_name = 'first_table';
+SHOW CREATE TABLE first_table;
