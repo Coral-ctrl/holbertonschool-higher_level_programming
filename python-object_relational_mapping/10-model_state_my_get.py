58cf1c15abacdfc1ec408e1690b9bf66f6c65158
“Shan” <liangyongshan33@gmail.com>
2026-03-05 06:10:24 +0000

    use filter

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter_by(
-        State.name=sys.argv[4]
+    state = session.query(State).filter(
+        State.name == sys.argv[4]
     ).first()
 
     if state is None:
