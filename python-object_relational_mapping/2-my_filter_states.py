2640926cf2b2fd1a2b48ece31ca7d5f7b5a753d8
“Shan” <liangyongshan33@gmail.com>
2026-03-05 00:06:49 +0000

    Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        search=sys.argv[4]
+    )
+
+    # Create a cursor object that sends SQL commands and receives results
+    cursor = db.cursor()
+    # Execute query
+    cursor.execute(
+        "SELECT * FROM states WHERE name = search ORDER BY id ASC"
+    )
+    # Fetch and display results
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
