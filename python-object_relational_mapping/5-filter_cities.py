f8ecd498c5dc6480aa1ec31ce83850315a88760f
“Shan” <liangyongshan33@gmail.com>
2026-03-05 03:26:32 +0000

    change logic

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -25,8 +25,7 @@ if __name__ == "__main__":
     )
 
     rows = cursor.fetchall()
-    for row in rows:
-        print(", ".join(row[0]))
+    print(", ".join(row[0] for row in rows))
 
     cursor.close()
     db.close()
