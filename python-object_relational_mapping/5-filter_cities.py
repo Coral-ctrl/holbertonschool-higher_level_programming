d75d8f8561fcc447d738f15d622f3f520732a1e4
“Shan” <liangyongshan33@gmail.com>
2026-03-05 03:50:34 +0000

    remove trailing whitespace

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cursor.execute(
         "SELECT cities.name FROM cities JOIN states "
         "ON cities.state_id = states.id "
-        "WHERE states.name LIKE BINARY %s ORDER BY cities.id ASC", 
+        "WHERE states.name = %s ORDER BY cities.id ASC",
         (sys.argv[4],)
     )
 
