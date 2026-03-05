ecfb09794b5e906ff2ff82b6ba1f736aa13d0111
“Shan” <liangyongshan33@gmail.com>
2026-03-05 03:14:32 +0000

    SQL injection free!

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -18,9 +18,10 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute(
-        f"SELECT cities.name "
+        "SELECT cities.name "
         "FROM cities JOIN states ON cities.state_id = states.id "
-        "WHERE states.name LIKE BINARY {sys.argv[4]} ORDER BY cities.id ASC"
+        "WHERE states.name = %s ORDER BY cities.id ASC",
+        (sys.argv[4],)
     )
 
     rows = cursor.fetchall()
